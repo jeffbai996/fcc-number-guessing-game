4cbdc9b1e1819266aa1d8e7026df093275ce749f
jeffbai996 <j.bai@wustl.edu>
2022-10-27 10:21:30 -0700

    Updated shell script with project progress

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-RANDOM = $RANDOM
+# add psql variable
+PSQL="psql -X --username=freecodecamp --dbname=games --tuples-only -c"
+
+# generate random number
+RAND_NUM=$(( $RANDOM % 1000 ))
+
+echo "Enter your username: "
+read USERNAME
 

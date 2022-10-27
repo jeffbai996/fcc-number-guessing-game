a7a44a45d30a6b4926d4c7bb9cc3720d09f894c5
jeffbai996 <j.bai@wustl.edu>
2022-10-27 10:28:13 -0700

    Updated shell script with project progress

--- number_guess.sh
+++ number_guess.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # add psql variable
-PSQL="psql -X --username=freecodecamp --dbname=games --tuples-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=game --tuples-only -c"
 
 # generate random number
 RAND_NUM=$(( $RANDOM % 1000 ))
 
+# get username
 echo "Enter your username: "
 read USERNAME
 
+# find user
+USER=$($PSQL "SELECT * FROM users WHERE username = $USERNAME::TEXT")
+
+# if not found
+if [[ -z $USER ]]
+then
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+fi
